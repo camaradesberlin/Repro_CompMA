f938ba1756ccb56ebbdba4833b9acd584e49a6d0
Funkstille1011 <torsten.rackoll@bih-charite.de>
2024-08-27 12:49:00 +0200

    edited script a bit

--- scripts/01_Preprocessing.R
+++ scripts/01_Preprocessing.R
@@ -15,7 +15,41 @@ str(df)
 colnames(df)
 
 df <- df %>%
-  select(-'Method of infarct measurement (latest method used)', 'Was infarct volume provided or calculated?') %>%
+  select(-c('Method of infarct measurement (latest method used)', 'Was infarct volume provided or calculated?',
+         'If calculated, what figure was volume obtained from?')) %>%
   rename(
-    
-  )
+    "Author" = "First Author (Year)",
+    "Condition" = "Control vs RIC",
+    "Int.Label" = "Intervention Group",
+    "Time.Inf" = "Latest timepoint (days)",
+    "Unit" = "Unit of measurement",
+    "Ref.vol" = "Reference point for infarct volume (% of total brain/hemisphere)",
+    "Type.RIC" = "Type of RIC",
+    "Time.cond" = "Pre-conditioning or post-conditioning?",
+    "Time.RIC" = "Time of RIC onset relative to onset of stroke (hours)",
+    "No.Limbs" = "Number of limbs used for RIC?",
+    "Limbs" = "Which limbs were used for RIC?",
+    "No.Session" = "Numberof RIC sessions",
+    "No.Cycles" = "Number of RIC cycles",
+    "Occlusion" = "Duration of occlusion (minutes)",
+    "Reperfusion" = "Duration of reperfusion (minutes)",
+    "Stroke.Type" = "Stroke type (permanent or reperfusion model)",
+    "Model" = "Stroke type",
+    "Occlusion.time" = "Occlusion time (min)"
+  ) %>%  mutate(across(c(
+    Anesthesia, Species, Condition, Ref.vol, Type.RIC,
+    Time.cond, Limbs, Sex, Stroke.Type, Model)
+    , as_factor),
+    across(c(
+      Mean, Median, Time.RIC
+    ), as.numeric
+           ),
+    across(c(
+      n, No.Limbs, No.Session, No.Cycles, Occlusion, Reperfusion, Occlusion.time
+    ), as.integer))) %>%# warning: NA's introduced by coercion. 
+     fill(RefID, Author, Species
+    )
+
+
+df.wide <- df %>%
+  group_by(RefID)
